09896e365ed2a6e11228d20567b2aee74f684a0c
Mehdi <mahdikh.76.mkh@gmail.com>
2026-07-19 15:47:35 +0330

    Update __init__.py

--- STVT/STVT/datasets/__init__.py
+++ STVT/STVT/datasets/__init__.py
@@ -9,15 +9,18 @@ from .SumMe_RFR_Normalized import SumMe_RFR_Normalized  #with normalized feature
 #   I3D RGB:  41.9% 
 #   I3D Flow: 26.5%  
 
-from .TVSum_i3d_FR import TVSum_i3d_FR
-from .TVSum_Rgb_Flow_Resnet import TVSum_Rgb_Flow_Resnet
-from .TVSum_Rgb_Flow import TVSum_Rgb_Flow
+from .TVSum_Resnet_Rgb import TVSum_Resnet_Rgb
+from .TVSum_Resnet_Flow import TVSum_Resnet_Flow
 
+
+from .TVSum_Rgb_Flow_Resnet import TVSum_Rgb_Flow_Resnet
 
 from .TVSum_RFR_Normalized import TVSum_RFR_Normalized
 
 
-__all__ = ['TVSum','SumMe','SumMe_i3d_FR','TVSum_i3d_FR',
-           'TVSum_Rgb_Flow_Resnet','SumMe_Rgb_Flow_Resnet','TVSum_Rgb_Flow','SumMe_Rgb_Flow_Resnet_matched','SumMe_normalized_concat_rfr',
-           'SumMe_RFR_Normalized','TVSum_RFR_Normalized']
+__all__ = ['TVSum','SumMe','SumMe_i3d_FR',
+           'TVSum_Rgb_Flow_Resnet','SumMe_Rgb_Flow_Resnet',
+           'SumMe_Rgb_Flow_Resnet_matched','SumMe_normalized_concat_rfr',
+           'SumMe_RFR_Normalized',
+           'TVSum_RFR_Normalized','TVSum_Resnet_Flow''TVSum_Resnet_Rgb',]
 
